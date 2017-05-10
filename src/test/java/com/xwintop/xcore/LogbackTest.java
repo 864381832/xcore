06249fd4ad676d2c7501b2b866cffc73554a64ab
864381832@qq.com <864381832@qq.com>
2017-05-11 00:23:06 +0800

    1、添加MyLogger工具类；2、添加权限管理工具包

--- src/test/java/com/xwintop/xcore/LogbackTest.java
+++ src/test/java/com/xwintop/xcore/LogbackTest.java
@@ -4,10 +4,18 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.xwintop.xcore.util.MyLogger;
+
 public class LogbackTest {
 	@Test
 	public void testLogger() {
 		Logger log = LoggerFactory.getLogger(LogbackTest.class);
-		log.info("ceshi");
+		log.info("test");
+	}
+	
+	@Test
+	public void testMyLogger() {
+		MyLogger log = new MyLogger(LogbackTest.class);
+		log.info("testMyLogger");
 	}
 }
---
+++ src/test/java/com/xwintop/xcore/ShiroTest.java
@@ -0,0 +1,34 @@
+package com.xwintop.xcore;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.junit.Test;
+
+public class ShiroTest {
+	@Test
+	public void testHelloworld() {
+		// 1、获取SecurityManager工厂，此处使用Ini配置文件初始化SecurityManager
+		Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini");
+		// 2、得到SecurityManager实例 并绑定给SecurityUtils
+		SecurityManager securityManager = factory.getInstance();
+		SecurityUtils.setSecurityManager(securityManager);
+		// 3、得到Subject及创建用户名/密码身份验证Token（即用户身份/凭证）
+		Subject subject = SecurityUtils.getSubject();
+		UsernamePasswordToken token = new UsernamePasswordToken("zhang", "123");
+		try {
+			// 4、登录，即身份验证
+			subject.login(token);
+		} catch (AuthenticationException e) {
+			// 5、身份验证失败
+		}
+		System.out.println(subject.isAuthenticated());
+		// Assert.assertEquals(true, subject.isAuthenticated()); // 断言用户已经登录
+		// 6、退出
+		subject.logout();
+	}
+}
