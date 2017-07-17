8e028ad4d2de3e6756a2fa6316930f6c26d1d972
XUFENG-PC\xufeng <1277032935@qq.com>
2017-07-17 17:45:38 +0800

    添加EasyMock测试包

---
+++ src/test/java/com/xwintop/xmytest/easyMock/package-info.java
@@ -0,0 +1,18 @@
+/**   
+ * Copyright © 2017 公司名. All rights reserved.
+ * 
+ * @Title: package-info.java 
+ * @Prject: xcore
+ * @Package: com.xwintop.xmytest.easyMock 
+ * @Description: TODO
+ * @author: xufeng   
+ * @date: 2017年7月17日 下午3:51:09 
+ * @version: V1.0   
+ */
+/** 
+ * @ClassName: package-info 
+ * @Description: EasyMock测试类
+ * @author: xufeng
+ * @date: 2017年7月17日 下午3:51:09  
+ */
+package com.xwintop.xmytest.easyMock;
