74b04c15dbe8a32f5d600a88919a04d7a8b5eaff
XUFENG-PC\xufeng <1277032935@qq.com>
2017-07-18 17:54:15 +0800

    添加javaFx测试

---
+++ src/test/java/com/xwintop/javaFX/Main.java
@@ -0,0 +1,38 @@
+package com.xwintop.javaFX;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+//		try {
+//			BorderPane root = new BorderPane();
+//			Scene scene = new Scene(root, 400, 400);
+//			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+//			primaryStage.setScene(scene);
+//			primaryStage.show();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+
+		try {
+			// Read file fxml and draw interface.
+			Parent root = FXMLLoader.load(getClass().getResource("/com/xwintop/javaFX/MyScene.fxml"));
+			primaryStage.setTitle("My Application");
+			primaryStage.setScene(new Scene(root));
+			primaryStage.show();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
