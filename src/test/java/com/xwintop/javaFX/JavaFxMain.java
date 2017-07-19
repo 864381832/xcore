06e46a2ac592d403dbc29d300e4071045656d7ab
XUFENG-PC\xufeng <1277032935@qq.com>
2017-07-19 17:39:29 +0800

    添加国际化测试类

--- src/test/java/com/xwintop/javaFX/JavaFxMain.java
+++ src/test/java/com/xwintop/javaFX/JavaFxMain.java
@@ -1,13 +1,14 @@
 package com.xwintop.javaFX;
 
+import org.junit.Test;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
-public class Main extends Application {
+public class JavaFxMain extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 //		try {
@@ -32,7 +33,8 @@ public class Main extends Application {
 		}
 	}
 
-	public static void main(String[] args) {
-		launch(args);
+	@Test
+	public void launchMain() {
+		JavaFxMain.launch(null);
 	}
 }
---
+++ src/test/java/com/xwintop/javaFX/locale/package-info.java
@@ -0,0 +1,18 @@
+/**   
+ * Copyright © 2017 公司名. All rights reserved.
+ * 
+ * @Title: package-info.java 
+ * @Prject: xcore
+ * @Package: com.xwintop.javaFX.locale 
+ * @Description: TODO
+ * @author: xufeng   
+ * @date: 2017年7月19日 上午9:34:52 
+ * @version: V1.0   
+ */
+/** 
+ * @ClassName: package-info 
+ * @Description: javaFx国际化测试
+ * @author: xufeng
+ * @date: 2017年7月19日 上午9:34:52  
+ */
+package com.xwintop.javaFX.locale;
