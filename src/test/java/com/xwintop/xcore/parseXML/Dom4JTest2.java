dd9d99b1aeecd9b9b06c1d3c6a93772373964553
XUFENG-PC\xufeng <1277032935@qq.com>
2017-09-14 18:12:29 +0800

    添加Dom4j测试

--- src/test/java/com/xwintop/xcore/parseXML/Dom4JTest2.java
+++ src/test/java/com/xwintop/xcore/parseXML/Dom4JTest2.java
@@ -1,17 +1,25 @@
 package com.xwintop.xcore.parseXML;
 
 import java.io.File;
+import java.lang.reflect.Field;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
+import java.util.function.Consumer;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
+import org.apache.commons.lang3.time.DateUtils;
 import org.dom4j.Document;
 import org.dom4j.Element;
 import org.dom4j.io.DOMReader;
 import org.dom4j.io.SAXReader;
 import org.junit.Test;
+
+import com.xwintop.xcore.base.Bill;
+
+import oracle.net.aso.f;
 
 /**
  * dom4j框架学习： 读取并解析xml
@@ -66,6 +74,40 @@ public class Dom4JTest2 {
 		Element rootElement = document3.getRootElement();
 
 		System.out.println("Root: " + rootElement.getName());
+
+	}
+	
+	@Test
+	public void TestDOMReader2() throws Exception {
+//		String date = "2016-05-06T00:57:02.227+01:00";
+		String date = "2016-05-06T00:57:02.227+01:00";
+		Date date2 = DateUtils.parseDate(date.split("+")[0], "yyyy-MM-dd'T'HH:mm:ss.SSS zz:z");
+		System.out.println(date2.toLocaleString());
+		Field[] fields = Bill.class.getDeclaredFields();
+		for(Field field:fields){
+			System.out.println(field.getType().getName());
+		}
+		SAXReader saxReader = new SAXReader();
+		Document document = saxReader.read(new File("D://港口信息PortsData.xml"));
+		// 获取根元素
+		Element root = document.getRootElement();
+		System.out.println("Root: " + root.getName());
+		// 获取所有子元素
+		@SuppressWarnings("unchecked")
+		List<Element> childList = root.elements();
+		System.out.println("total child count: " + childList.size());
+		// 获取特定名称的子元素
+		@SuppressWarnings("unchecked")
+		List<Element> childList2 = root.elements("PortsData");
+		System.out.println("hello child: " + childList2.size());
+		for(Element element1:childList2){
+			element1.elements().forEach(new Consumer<Element>() {
+				@Override
+				public void accept(Element element) {
+					System.out.println(element.getName()+":"+element.getText());
+				}
+			});
+		}
 
 	}
 
