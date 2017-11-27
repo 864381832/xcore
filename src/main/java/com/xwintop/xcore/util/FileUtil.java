bc33c0eba81b0521018829f4c3a65ad55a80701c
1277032935@qq.com <1277032935@qq.com>
2017-11-28 00:00:03 +0800

    添加file文件大小转换方法。

--- src/main/java/com/xwintop/xcore/util/FileUtil.java
+++ src/main/java/com/xwintop/xcore/util/FileUtil.java
@@ -1,6 +1,7 @@
 package com.xwintop.xcore.util;
 
 import java.io.File;
+import java.text.DecimalFormat;
 
 public class FileUtil {
 	public static String[] getFileNames(File file) {
@@ -42,4 +43,25 @@ public class FileUtil {
 		}
 		return fileName;
 	}
+
+    /**
+     * 转换文件的大小以B,KB,M,G等计算
+     *
+     * @param fileS
+     * @return
+     */
+    public static String FormetFileSize(long fileS) {// 转换文件大小
+        DecimalFormat df = new DecimalFormat("#.000");
+        String fileSizeString = "";
+        if (fileS < 1024) {
+            fileSizeString = df.format((double) fileS) + "B";
+        } else if (fileS < 1048576) {
+            fileSizeString = df.format((double) fileS / 1024) + "K";
+        } else if (fileS < 1073741824) {
+            fileSizeString = df.format((double) fileS / 1048576) + "M";
+        } else {
+            fileSizeString = df.format((double) fileS / 1073741824) + "G";
+        }
+        return fileSizeString;
+    }
 }
