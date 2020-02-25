e471091d66ecc1508e5e17aeb341b6eb653141ea
xufeng <1277032935@qq.com>
2020-02-25 13:58:41 +0800

    1、更新版本为0.0.3；2、移除多余引用的包；3、移除部分工具类

---
+++ README.md
@@ -0,0 +1,22 @@
+封装java开发过程中常用的工具类。
+
+git托管maven可参考教程(若无法下载请拉取项目自行编译)。[教程地址：点击进入](http://blog.csdn.net/u011747754/article/details/78574026)
+
+访问地址：
+```
+<repositories>
+	<repository>
+		<id>xwintop-maven</id>
+		<url>https://xwintop.gitee.io/maven/repository</url>
+	</repository>
+</repositories>
+
+<dependencies>
+	<dependency>
+		<groupId>com.xwintop</groupId>
+		<artifactId>xcore</artifactId>
+		<version>0.0.3-SNAPSHOT</version>
+		<scope>provided</scope>
+	</dependency>
+</dependencies>
+```
--- src/main/java/com/xwintop/xcore/commons/CacheUtil.java
+++ src/main/java/com/xwintop/xcore/commons/CacheUtil.java
@@ -1,11 +1,10 @@
 package com.xwintop.xcore.commons;
 
-import com.github.benmanes.caffeine.cache.Cache;
-import com.github.benmanes.caffeine.cache.Caffeine;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.concurrent.TimeUnit;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 /**
@@ -18,7 +17,8 @@ import java.util.function.Function;
 @Setter
 public class CacheUtil {
     private static CacheUtil cacheUtil = new CacheUtil();
-    private Cache cache = Caffeine.newBuilder().expireAfterWrite(12, TimeUnit.HOURS).build();
+    //    private Cache cache = Caffeine.newBuilder().expireAfterWrite(12, TimeUnit.HOURS).build();
+    private Map cache = new HashMap();
 
     public static CacheUtil getInstance() {
         return cacheUtil;
@@ -29,18 +29,24 @@ public class CacheUtil {
     }
 
     public Object get(Object key) {
-        return cache.getIfPresent(key);
+//        return cache.getIfPresent(key);
+        return cache.get(key);
     }
 
     public <T> T get(Object key, Class<T> type) {
-        return (T) cache.getIfPresent(key);
+//        return (T) cache.getIfPresent(key);
+        return (T) cache.get(key);
     }
 
     public <T> T get(Object key, Class<T> type, Function mappingFunction) {
-        return (T) cache.get(key, mappingFunction);
+        if (get(key, type) == null) {
+            set(key, mappingFunction.apply(null));
+        }
+        return (T) cache.get(key);
     }
 
     public void remove(Object key) {
-        cache.invalidate(key);
+//        cache.invalidate(key);
+        cache.remove(key);
     }
 }
