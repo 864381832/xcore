9a8143db202e621782dd58cb55c91e6a01f8472d
He Yiding <yiding.he@gmail.com>
2020-02-27 13:43:46 +0800

    新增 IntegerSpinner 和 DoubleSpinner

--- src/main/java/com/xwintop/xcore/javafx/control/IntegerSpinner.java
+++ src/main/java/com/xwintop/xcore/javafx/control/IntegerSpinner.java
@@ -2,7 +2,11 @@ package com.xwintop.xcore.javafx.control;
 
 import javafx.beans.NamedArg;
 import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory.IntegerSpinnerValueFactory;
 
+/**
+ * 只接受整数的 Spinner，增强方面：可以直接赋值，可以获取最小值最大值
+ */
 public class IntegerSpinner extends Spinner<Integer> {
 
     public IntegerSpinner(
@@ -12,5 +16,30 @@ public class IntegerSpinner extends Spinner<Integer> {
         @NamedArg(value = "amountToStepBy", defaultValue = "1") int amountToStepBy
     ) {
         super(min, max, initialValue, amountToStepBy);
+    }
+
+    /**
+     * 赋值，当 value 超出最大最小值范围时抛出异常
+     */
+    public void setValue(int value) {
+        IntegerSpinnerValueFactory valueFactory = getSpinnerValueFactory();
+        if (value < valueFactory.getMin() || value > valueFactory.getMax()) {
+            throw new IllegalArgumentException(
+                "Value out of bounds(" + valueFactory.getMin() + "~" + valueFactory.getMax() + ")."
+            );
+        }
+        valueFactory.setValue(value);
+    }
+
+    public IntegerSpinnerValueFactory getSpinnerValueFactory() {
+        return (IntegerSpinnerValueFactory) getValueFactory();
+    }
+
+    public int getMin() {
+        return getSpinnerValueFactory().getMin();
+    }
+
+    public int getMax() {
+        return getSpinnerValueFactory().getMax();
     }
 }
