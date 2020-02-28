4e9bf9831773fd8598f525f611cbaef57471a24e
Yiding He <yiding.he@gmail.com>
2020-02-28 19:50:30 +0800

    FxmlHelper 添加方法

--- src/main/java/com/xwintop/xcore/javafx/helper/FxmlHelper.java
+++ src/main/java/com/xwintop/xcore/javafx/helper/FxmlHelper.java
@@ -7,14 +7,28 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class FxmlHelper {
 
+    // 根据路径加载 FXML 资源，并返回 FXMLLoader 对象
     public static FXMLLoader loadFromResource(String resourcePath) {
+        return loadFromResource(resourcePath, null);
+    }
+
+    // 根据路径和 resourceBundleName 加载 FXML 资源，并返回 FXMLLoader 对象
+    public static FXMLLoader loadFromResource(String resourcePath, String resourceBundleName) {
         try {
-            FXMLLoader fxmlLoader = new FXMLLoader(
-                    FxmlUtil.class.getResource(resourcePath)
-            );
+            FXMLLoader fxmlLoader;
+            URL url = FxmlUtil.class.getResource(resourcePath);
+
+            if (resourceBundleName != null) {
+                fxmlLoader = new FXMLLoader(url, ResourceBundle.getBundle(resourceBundleName));
+            } else {
+                fxmlLoader = new FXMLLoader(url);
+            }
+
             fxmlLoader.load();
             return fxmlLoader;
         } catch (IOException e) {
@@ -23,8 +37,12 @@ public class FxmlHelper {
     }
 
     public static Stage loadIntoStage(String fxml, Stage stage) {
+        return loadIntoStage(fxml, stage, null);
+    }
+
+    public static Stage loadIntoStage(String fxml, Stage stage, String resourceBundleName) {
         stage.setScene(new Scene(
-                loadFromResource(fxml).getRoot()
+            loadFromResource(fxml, resourceBundleName).getRoot()
         ));
         return stage;
     }
