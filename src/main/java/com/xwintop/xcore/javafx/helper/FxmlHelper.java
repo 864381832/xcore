90c7b1017a6ab6a935715dbe79692a5af648bcc0
He Yiding <yiding.he@gmail.com>
2020-02-29 17:18:08 +0800

    FxmlHelper 添加方法

--- src/main/java/com/xwintop/xcore/javafx/helper/FxmlHelper.java
+++ src/main/java/com/xwintop/xcore/javafx/helper/FxmlHelper.java
@@ -1,20 +1,34 @@
 package com.xwintop.xcore.javafx.helper;
 
 import com.xwintop.xcore.XCoreException;
-import com.xwintop.xcore.util.javafx.FxmlUtil;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
+import org.apache.commons.lang3.StringUtils;
 
 public class FxmlHelper {
 
     public static FXMLLoader loadFromResource(String resourcePath) {
+        return loadFromResource(resourcePath, null);
+    }
+
+    public static FXMLLoader loadFromResource(String resourcePath, String bundleName) {
         try {
-            FXMLLoader fxmlLoader = new FXMLLoader(
-                    FxmlUtil.class.getResource(resourcePath)
-            );
+            URL resource = FxmlHelper.class.getResource(resourcePath);
+            if (resource == null) {
+                throw new IllegalArgumentException("invalid fxml path " + resourcePath);
+            }
+
+            FXMLLoader fxmlLoader;
+            if (StringUtils.isNotBlank(bundleName)) {
+                fxmlLoader = new FXMLLoader(resource, ResourceBundle.getBundle(bundleName));
+            } else {
+                fxmlLoader = new FXMLLoader(resource);
+            }
+
             fxmlLoader.load();
             return fxmlLoader;
         } catch (IOException e) {
@@ -22,10 +36,13 @@ public class FxmlHelper {
         }
     }
 
+    public static Stage loadIntoStage(String fxml, String bundleName, Stage stage) {
+        stage.setScene(new Scene(loadFromResource(fxml, bundleName).getRoot()));
+        return stage;
+    }
+
     public static Stage loadIntoStage(String fxml, Stage stage) {
-        stage.setScene(new Scene(
-                loadFromResource(fxml).getRoot()
-        ));
+        stage.setScene(new Scene(loadFromResource(fxml).getRoot()));
         return stage;
     }
 }
