187afc2a48eece251e7b0c30aee90c2c50a43f36
xufeng <1277032935@qq.com>
2021-03-04 10:50:13 +0800

    添加Jpro支持

--- src/main/java/com/xwintop/xcore/util/javafx/JavaFxSystemUtil.java
+++ src/main/java/com/xwintop/xcore/util/javafx/JavaFxSystemUtil.java
@@ -5,6 +5,8 @@ import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.io.File;
 import java.io.IOException;
+
+import com.jpro.webapi.WebAPI;
 import javafx.geometry.Rectangle2D;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
@@ -21,6 +23,7 @@ public class JavaFxSystemUtil {
 
     /**
      * 打开目录
+     *
      * @param directoryPath 目录路径
      */
     public static void openDirectory(String directoryPath) {
@@ -31,14 +34,17 @@ public class JavaFxSystemUtil {
         }
     }
 
-
     /**
      * 获取屏幕尺寸
+     *
      * @param width  宽度比
      * @param height 高度比
      * @return 屏幕尺寸
      */
     public static double[] getScreenSizeByScale(double width, double height) {
+        if (WebAPI.isBrowser()) {
+            return new double[]{1280, 900};
+        }
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         double screenWidth = screenSize.width * width;
         double screenHeight = screenSize.height * height;
--- src/main/java/com/xwintop/xcore/util/javafx/JavaFxViewUtil.java
+++ src/main/java/com/xwintop/xcore/util/javafx/JavaFxViewUtil.java
@@ -3,6 +3,7 @@ package com.xwintop.xcore.util.javafx;
 import cn.hutool.cache.impl.TimedCache;
 import cn.hutool.core.lang.Singleton;
 import com.jfoenix.controls.JFXDecorator;
+import com.jpro.webapi.WebAPI;
 import com.xwintop.xcore.javafx.FxApp;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -210,8 +211,13 @@ public class JavaFxViewUtil {
     public static void openNewWindow(String title, String iconUrl, Parent root, double width, double height,
                                      boolean fullScreen, boolean max, boolean min) {
         Stage newStage = getNewStageNull(title, iconUrl, root, width, height, fullScreen, max, min);
-        newStage.initModality(Modality.APPLICATION_MODAL);
-        newStage.show();
+        if (WebAPI.isBrowser()) {
+            WebAPI webAPI = WebAPI.getWebAPI(JavaFxSystemUtil.mainStage);
+            webAPI.openStageAsTab(newStage);
+        } else {
+            newStage.initModality(Modality.APPLICATION_MODAL);
+            newStage.show();
+        }
     }
 
     //获取一个新窗口
