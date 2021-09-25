d8590bacaa14275a056c9f89ead1af1bcea249aa
xufeng <1277032935@qq.com>
2021-09-25 21:12:11 +0800

    添加Image转换方法

--- src/main/java/com/xwintop/xcore/util/javafx/ImageUtil.java
+++ src/main/java/com/xwintop/xcore/util/javafx/ImageUtil.java
@@ -2,8 +2,8 @@ package com.xwintop.xcore.util.javafx;
 
 import com.xwintop.xcore.util.FileUtil;
 import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.image.*;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import lombok.Getter;
 import lombok.Setter;
 import org.apache.commons.imaging.ImageFormats;
@@ -12,6 +12,7 @@ import org.apache.commons.imaging.Imaging;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.filechooser.FileSystemView;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.File;
@@ -115,5 +116,74 @@ public class ImageUtil {
         icon.paintIcon(null, bufferedImage.getGraphics(), 0, 0);
         Image fxImage = SwingFXUtils.toFXImage(bufferedImage, null);
         return new ImageView(fxImage);
+    }
+
+    /**
+     * 根据传入类型变化图片像素
+     */
+    public static javafx.scene.image.Image pixWithImage(int type, javafx.scene.image.Image image) {
+        PixelReader pixelReader = image.getPixelReader();
+        if (image.getWidth() > 0 && image.getHeight() > 0) {
+            WritableImage wImage;
+            wImage = new WritableImage((int) image.getWidth(), (int) image.getHeight());
+            PixelWriter pixelWriter = wImage.getPixelWriter();
+
+            for (int y = 0; y < image.getHeight(); y++) {
+                for (int x = 0; x < image.getWidth(); x++) {
+                    javafx.scene.paint.Color color = pixelReader.getColor(x, y);
+                    switch (type) {
+                        case 1:
+                            // 颜色变轻
+                            color = color.brighter();
+                            break;
+                        case 2:
+                            // 颜色变深
+                            color = color.darker();
+                            break;
+                        case 3:
+                            // 灰度化
+                            color = color.grayscale();
+                            break;
+                        case 4:
+                            // 颜色反转
+                            color = color.invert();
+                            break;
+                        case 5:
+                            // 颜色饱和
+                            color = color.saturate();
+                            break;
+                        case 6:
+                            // 颜色不饱和
+                            color = color.desaturate();
+                            break;
+                        case 7:
+                            // 颜色灰度化后反转（字黑体，背景鲜亮，可用于强字弱景）
+                            color = color.grayscale();
+                            color = color.invert();
+                            break;
+                        case 8:
+                            // 颜色透明
+                            if (color.getOpacity() == 0) {
+                                color = new javafx.scene.paint.Color(color.getRed(), color.getGreen(), color.getBlue(), 0);
+                            } else {
+                                color = new javafx.scene.paint.Color(color.getRed(), color.getGreen(), color.getBlue(), 0.5);
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+
+                    pixelWriter.setColor(x, y, color);
+                }
+            }
+            return wImage;
+        }
+        return null;
+    }
+
+    //将javafx中Color转换为awt中Color
+    public static java.awt.Color getAwtColor(javafx.scene.paint.Color color) {
+        java.awt.Color colorw = new Color((float) color.getRed(), (float) color.getGreen(), (float) color.getBlue(), (float) color.getOpacity());
+        return colorw;
     }
 }
